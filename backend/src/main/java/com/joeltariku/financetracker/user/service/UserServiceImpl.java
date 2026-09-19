18195f5e937c81c191ebb4182f19eb2826b96bc7
Joel Tariku <jojotk10@gmail.com>
2026-09-19 18:25:00 -0400

    Implemented the UserService interface.

---
+++ backend/src/main/java/com/joeltariku/financetracker/user/service/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.joeltariku.financetracker.user.service;
+
+import com.joeltariku.financetracker.user.CreateUserRequest;
+import com.joeltariku.financetracker.user.User;
+import com.joeltariku.financetracker.user.UserRepository;
+
+import java.util.Optional;
+
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User createUser(CreateUserRequest request) {
+        User user = new User(
+                request.firstName(),
+                request.lastName(),
+                request.username(),
+                request.email(),
+                request.passwordHash()
+        );
+
+        return userRepository.save(user);
+    }
+
+    @Override
+    public Optional<User> findByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> findByUsername(String username) {
+        return Optional.empty();
+    }
+}
